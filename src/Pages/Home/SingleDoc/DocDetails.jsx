1179190838dd4c00d6cbc9a29934317035aad085
bijoymamud <bijoymamud.09@gmail.com>
2023-11-03 11:55:54 +0600

    tab of about

--- src/Pages/Home/SingleDoc/DocDetails.jsx
+++ src/Pages/Home/SingleDoc/DocDetails.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FaStar } from "react-icons/fa";
 import { useParams } from 'react-router-dom';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import docDetails from "../../../../public/doctors.json";
 const DocDetails = () => {
 
@@ -15,20 +17,97 @@ const DocDetails = () => {
 
       <div className='flex gap-10 mx-auto'>
 
-        <div className='flex gap-5 '>
-          <div>
-            <img className='h-[200px] w-[200px]' src={info.img} alt="" />
+        <div>
+          <div className='flex gap-5 '>
+            <div>
+              <img className='h-[200px] w-[200px]' src={info.img} alt="" />
+            </div>
+
+            <div className='text-start '>
+              <button className="btn btn-sm mb-2 hover:bg-sky-200 bg-sky-200 mt-2">{info.specialty}</button>
+              <div className='py-3'>
+                <h2 className='card-title mt-1'>{info.name}</h2>
+                <p className='flex items-center gap-1 my-1'><FaStar className='text-orange-500' /><span className='font-bold'>{info.avgRating}</span><span className='ms-1 text-gray-400'>({info.totalRating})</span></p>
+              </div>
+              <p className='text-gray-500 pt-3 w-2/3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            </div>
+
           </div>
 
-          <div className='text-start '>
-            <button className="btn btn-sm mb-2 hover:bg-sky-200 bg-sky-200 mt-2">{info.specialty}</button>
-            <div className='py-3'>
-              <h2 className='card-title mt-1'>{info.name}</h2>
-              <p className='flex items-center gap-1 my-1'><FaStar className='text-orange-500' /><span className='font-bold'>{info.avgRating}</span><span className='ms-1 text-gray-400'>({info.totalRating})</span></p>
-            </div>
-            <p className='text-gray-500 pt-3 w-2/3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-          </div>
-        </div>
+
+          <div className='md:pt-20 '>
+            <Tabs>
+              <TabList>
+                <Tab>About</Tab>
+                <Tab>FeedBack</Tab>
+              </TabList>
+
+              <div className='md:pt-10 '>
+                <TabPanel>
+                  <div>
+                    <h2 className='text-lg'>About of <span className='text-primary font-extrabold ms-2 text-xl'>{info.name}</span></h2>
+
+                    <p className='pt-5 text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio facilis veritatis autem incidunt pariatur labore, rerum sunt laudantium voluptates nostrum ipsa error illo voluptatibus odit ipsam nemo ducimus, praesentium et.</p>
+
+                    <div className='md:pt-10 '>
+
+                      {/* <h1>Doctor's Educational Information</h1>
+                      <ul>
+                        {info?.map((doctor, index) => (
+                          <li key={index}>
+                            <strong>Doctor Name:</strong> {doctor.doctorName}
+                            <ul>
+                              {doctor.educationalInfo.map((education, subIndex) => (
+                                <li key={subIndex}>
+                                  <strong>Degree:</strong> {education.degree}<br />
+                                  <strong>Year Completed:</strong> {education.yearCompleted}<br />
+                                  <strong>Institution:</strong> {education.institution}
+                                </li>
+                              ))}
+                            </ul>
+                          </li>
+                        ))}
+                      </ul> */}
+                      <div className='py-10'>
+                        <p className='text-xl  font-extrabold pb-5'>{info.name}'s <span className='ms-2 text-black'>Education</span></p>
+                        <ul className='text-gray-500 mb-5 '>
+                          <span className='pb-20'>
+                            {info?.educationalInfo.map((plot) => (
+                              <li className='flex items-center justify-between gap-10 pb-6' key={plot.id}>
+                                {plot.yearCompleted} <br />{plot.degree}
+                                <div className='flex items-center text-pink-500 font-bold' >
+                                  {plot.institution}
+                                </div>
+                              </li>
+                            ))}
+                          </span>
+
+                        </ul>
+                      </div>
+
+
+
+                    </div>
+                  </div>
+
+
+
+                </TabPanel>
+
+              </div>
+
+
+              <div className='w-full'>
+                <TabPanel>
+                  <h2>Feedback content</h2>
+                </TabPanel>
+              </div>
+
+            </Tabs>
+          </div >
+
+
+        </div >
 
         <div>
           <div className=' p-5 shadow-xl rounded-md'>
@@ -54,9 +133,9 @@ const DocDetails = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div >
 
-    </section>
+    </section >
   );
 };
 
