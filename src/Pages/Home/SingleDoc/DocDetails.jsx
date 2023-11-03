d4eddbe2daf0b2aab2d70b23fb8771737470d1d2
bijoymamud <bijoymamud.09@gmail.com>
2023-11-03 10:48:03 +0600

    react tab for toggole

--- src/Pages/Home/SingleDoc/DocDetails.jsx
+++ src/Pages/Home/SingleDoc/DocDetails.jsx
@@ -10,25 +10,49 @@ const DocDetails = () => {
   const info = docDetails.find((detail) => detail.id == id)
 
   return (
-    <section className='container w-5/6
+    <section className='container w-4/6 md:py-32
     '>
 
-      <div className='flex justify-between'>
+      <div className='flex gap-10 mx-auto'>
 
-        <div>
+        <div className='flex gap-5 '>
           <div>
-            <img src={info.img} alt="" />
+            <img className='h-[200px] w-[200px]' src={info.img} alt="" />
           </div>
 
-          <div className='text-start'>
-            <h2>{info.specialty}</h2>
-            <h2>{info.name}</h2>
-            <p className='flex items-center gap-1'><FaStar className='text-orange-500' /><span className='font-bold'>{info.avgRating}</span><span className='ms-1 text-gray-400'>({info.totalRating})</span></p>
+          <div className='text-start '>
+            <button className="btn btn-sm mb-2 hover:bg-sky-200 bg-sky-200 mt-2">{info.specialty}</button>
+            <div className='py-3'>
+              <h2 className='card-title mt-1'>{info.name}</h2>
+              <p className='flex items-center gap-1 my-1'><FaStar className='text-orange-500' /><span className='font-bold'>{info.avgRating}</span><span className='ms-1 text-gray-400'>({info.totalRating})</span></p>
+            </div>
+            <p className='text-gray-500 pt-3 w-2/3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
           </div>
         </div>
 
         <div>
-          <h2>For visit</h2>
+          <div className=' p-5 shadow-xl rounded-md'>
+            <div className=' flex items-center justify-between gap-20'>
+              <h1 className='text-gray-600 font-semibold'>Ticket Price</h1>
+              <h1 className='font-extrabold  card-title'>{info.visitPrice}</h1>
+            </div>
+
+            <div >
+              <h2 className='font-extrabold mt-10 pb-2 '>Available Time Slots:</h2>
+
+              <ul className='text-gray-500'>
+                {info?.visitingHours.map((slot) => (
+                  <li className='flex items-center justify-between gap-10 py-1' key={slot.day}>
+                    {slot.day}: <span className='font-bold'>{slot.hours}</span>
+                  </li>
+                ))}
+              </ul>
+
+            </div>
+            <div className='text-center'>
+              <button className='bg-primary text-white btn btn-wide mt-5 hover:bg-green-600 hover:text-white'>BOOK APPOINMENT</button>
+            </div>
+          </div>
         </div>
       </div>
 
